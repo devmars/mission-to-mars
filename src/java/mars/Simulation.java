1cd1ee43bcdacdc76c2513a61314224276052bae
Albert Camacho <camacho.albert82@gmail.com>
2019-02-01 15:04:22 -0400

    UML created and added the base structure

---
+++ src/java/mars/Simulation.java
@@ -0,0 +1,25 @@
+package mars;
+
+import java.util.List;
+
+public class Simulation {
+
+    private List<Rocket> rockets;
+
+    public List<Item> loadItems(){
+        // implement code here
+        return null;
+    }
+
+    public void loadU1(){
+        // implement code here
+    }
+
+    public void loadU2(){
+        // implement code here
+    }
+
+    public void runSimulation(){
+        // implement code here
+    }
+}
